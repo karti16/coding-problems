e10e4a137d278d6046383074f6adecaaccd78630
karti16 <kartim316@gmail.com>
2022-03-10 13:56:56 +0530

    TwoSums - leetcode added comments

--- Two Sum/python3-sol.py
+++ Two Sum/python3-sol.py
@@ -19,19 +19,25 @@ from collections import OrderedDict
 from collections import deque
 from collections import Counter
 
+# nums = [1, 3, 5, 9]
+# target = 8
+# declare dictionary
+# accessing index and its value from nums list
+# storing difference of target and values from the nums list
+# check the diff value already present in the dictionary or not. For first iteration it wont be there, since it is empty dict
+# if value present return the index stored in dict and iteration number (i)
+# storing the value and its index in dict
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # nums = [1, 3, 5, 9]
-        # target = 8
-        prevMap = {}
-
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
+        prevMap = {}  
+        for i, n in enumerate(nums): 
+            diff = target - n   
+            if diff in prevMap:   
+                return [prevMap[diff], i]  
+            prevMap[n] = i   
         return
 
 
 print(Solution().twoSum(nums=[2,7,11,15], target=9))
-print(Solution().twoSum(nums=[2,7,11,15], target=18))
