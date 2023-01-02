9e6377cf467af823eaec10911061d7d63dcad095
karti16 <kartim316@gmail.com>
2023-01-02 22:56:09 +0530

    linkedlist

--- Other problems/Java LinkedList/Demo.java
+++ Other problems/Java LinkedList/Demo.java
@@ -1,16 +1,14 @@
 public class Demo {
     public static void main(String[] args) {
 
-        LinkedList list1 = new LinkedList();
+        LinkedList<Integer> list1 = new LinkedList<Integer>();
         list1.insetAtBeginning(1);
         list1.insetAtBeginning(2);
         list1.insertAtPos(0, 99);
         list1.insertAtPos(0, 67);
 
-        list1.display();
-        System.out.println("");
-        list1.deleteAtPos(0);
-
-        list1.display();
+        for (int n : list1) {
+            System.out.print(n + " ");
+        }
     }
 }
