5740156b293520d414fc43621fcb63e4e90b3afe
karthikm <karthik.m@vttech.in>
2023-03-21 22:34:40 +0530

    pattern

--- test.java
+++ test.java
@@ -2,16 +2,19 @@
 public class test {
   public static void main(String[] args) {
     int n = 5;
-    int start;
     for (int i = 1; i <= n; i++) {
-      if (i % 2 == 0)
-        start = 0;
-      else
-        start = 1;
+      // numbers
       for (int j = 1; j <= i; j++) {
-        System.out.print(start + " ");
-        start = 1 - start;
+        System.out.print(j + " ");
       }
+
+      // space
+
+      // numbers
+      for (int j = i; j >= i; j--) {
+        System.out.print(j + " ");
+      }
+
       System.out.println("");
     }
   }
